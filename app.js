1dcccd436ab6d78396ead1d68a47034b57f3241a
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-03-06 11:37:29 -0500

    Comment out sample pg query.

--- app.js
+++ app.js
@@ -35,28 +35,28 @@ const pool = new pg.Pool({
   max: 10
 });
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(res.rows);
-  pool.end();
-});
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(res.rows);
+//   pool.end();
+// });
 
 // catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});*/
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 app.listen(3001, () => {
   console.log(`Server running on port 3001...`);
