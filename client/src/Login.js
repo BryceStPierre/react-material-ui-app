d935119371f3a043c927cd513b0c1fbde0c4a469
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-03-08 11:59:02 -0500

    Added controls to Login component.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,32 +1,122 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
-import Paper from 'material-ui/Paper';
+import classNames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+
 import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
-export default class Login extends Component {
+import Card, { CardContent } from 'material-ui/Card';
+
+import FaFacebook from 'react-icons/lib/fa/facebook';
+import FaGoogle from 'react-icons/lib/fa/google';
+import Divider from 'material-ui/Divider';
+
+const styles = theme => ({
+  container: {
+    //display: 'flex',
+    //flexWrap: 'wrap',
+  },
+  button: {
+    marginBottom: theme.spacing.unit,
+    //width: 'auto'
+  },
+  card: {
+    minWidth: 275,
+    maxWidth: 400,
+    margin: 'auto'
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  }
+});
+
+class Login extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      email: null,
+      password: null
+    };
+  }
+  
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state);
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   render() {
+    const { classes } = this.props;
     return (
-      <Paper elevation={4}>
-      <form >
-        <TextField
-          //id="password"
-          label="Username"
-          //className={classes.textField}
-          type="text"
-          autoComplete="current-password"
-          //margin="normal"
-        />
-        <br />
-        <TextField
-          //id="password"
-          label="Password"
-          //className={classes.textField}
-          type="password"
-          autoComplete="current-password"
-          margin="normal"
-        />
-      </form>
-      </Paper>
+      <Card className={classes.card}>
+        <CardContent>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              className={classes.button}
+              name="email"
+              label="Email"
+              type="text"
+              onChange={this.handleChange}
+              autoComplete="current-email"
+              fullWidth
+            />
+            <br />
+            <TextField
+              className={classes.button}
+              name="password"
+              label="Password"
+              type="password"
+              onChange={this.handleChange}
+              autoComplete="current-password"
+              fullWidth
+            />
+            <br />
+            <Button
+              className={classes.button}
+              variant="raised" 
+              color="primary"
+              fullWidth
+            >
+              Sign In
+            </Button>
+            <br />
+            <Button
+              className={classes.button}
+              variant="raised" 
+              color="secondary"
+              fullWidth
+            >
+              <FaGoogle className={classes.leftIcon} />
+              Sign In with Google
+            </Button>
+            <br />
+            <Button 
+              className={classes.button}
+              variant="raised" 
+              color="secondary"
+              fullWidth
+            >
+              <FaFacebook className={classes.leftIcon} />
+              Sign In with Facebook
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     );
   }
 }
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
