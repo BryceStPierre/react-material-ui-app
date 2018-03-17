90f73169239967a4afae560b1c4614deaf91551c
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-03-17 11:37:08 -0400

    Added sample GridList with transitions to Explore component.

--- client/src/Explore.js
+++ client/src/Explore.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { withStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
-import Subheader from 'material-ui/List/ListSubheader';
+import { CircularProgress } from 'material-ui/Progress';
 import IconButton from 'material-ui/IconButton';
 import InfoIcon from 'material-ui-icons/Info';
+
+import Grow from 'material-ui/transitions/Grow';
+
+import sample from './images/sample.png';
+const tileData = [
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample },
+  { title: 'Sample', author: 'Author', img: sample }
+];
 
 const styles = theme => ({
   root: {
     textAlign: 'center'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden'
+    //backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '100%',
+    textAlign: 'left'
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
 
@@ -22,7 +51,8 @@ class Explore extends Component {
     super(props);
   
     this.state = {
-      search: ''
+      search: '',
+      loading: false
     };
   }
   
@@ -37,7 +67,7 @@ class Explore extends Component {
 
   render() {
     const { classes } = this.props;
-    const { search } = this.state;
+    const { search, loading } = this.state;
 
     return (
       <div className={classes.root}>
@@ -60,9 +90,41 @@ class Explore extends Component {
           Clear
         </Button>
 
+        <div className={classes.container}>
+          <GridList 
+            className={classes.gridList} 
+            cellHeight={180}
+            cols={3}
+          >
+            <GridListTile key='Subheader' cols={3} style={{ height: 'auto' }}>
+              <Typography variant="title">
+                Results
+              </Typography>
+            </GridListTile>
 
-
-
+            {tileData.map((tile, i) => (
+              <Grow timeout={{ enter: i * 500, exit: i * 500 + 500 }}  in>
+                <GridListTile key={tile.img}>
+                  <img src={tile.img} alt={tile.title} />
+                  <GridListTileBar
+                    title={tile.title}
+                    subtitle={<span>by: {tile.author}</span>}
+                    actionIcon={
+                      <IconButton className={classes.icon}>
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </GridListTile>
+              </Grow>
+            ))}
+          </GridList>
+        </div>
+        { loading && <CircularProgress 
+          className={classes.progress}
+          color='primary'
+          size={75} />
+        }
       </div>
     );
   }
