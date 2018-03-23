54d54a40b691f04708b3e9ce981ff559adc42faa
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-03-23 17:55:18 -0400

    Updated CreateSkeam form.

--- client/src/components/forms/CreateSkeam.js
+++ client/src/components/forms/CreateSkeam.js
@@ -7,14 +7,25 @@ import TextField from 'material-ui/TextField';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
+import Switch from 'material-ui/Switch';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import {
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  FormHelperText,
+} from 'material-ui/Form';
 
 const styles = theme => ({
   button: {
     margin: theme.spacing.unit
   },
   paper: {
-    maxWidth: 500,
+    maxWidth: 400,
     padding: theme.spacing.unit * 2
+  },
+  gap: {
+    marginBottom: theme.spacing.unit * 2
   }
 });
 
@@ -24,7 +35,9 @@ class CreateSkeam extends Component {
 
     this.state = {
       title: null,
-      description: null
+      description: null,
+      category: 'miscellaneous',
+      access: true
     };
   }
 
@@ -34,63 +47,105 @@ class CreateSkeam extends Component {
     });
   };
 
+  handleSwitch = (e, checked) => {
+    this.setState({ [e.target.name]: checked });
+  };
+
   handleSubmit = (e) => {
-    e
+    e.preventDefault();
   };
   
   render() {
     const { classes } = this.props;
+    const { category, access } = this.state;
 
     return (
-      <Paper className={classes.paper}>
+      <div>
         <Typography 
+          className={classes.gap} 
           variant='subheading'
-          gutterBottom
         >
           Let's begin with a few details. Don't worry, you can change them later.
         </Typography>
-        <form onSubmit={this.handleSubmit}>
-          <TextField
-            name='title'
-            label='Title'
-            onChange={this.handleChange}
-            margin='dense'
-            fullWidth
-            required
-          />
-          <br />
-          <TextField
-            name='description'
-            label='Description'
-            onChange={this.handleChange}
-            margin='dense'
-            rowsMax={3}
-            multiline
-            fullWidth
-            required
-          />
-          <br />
-          <Typography align='center'>
-            <Button
-              type='submit'
-              className={classes.button}
-              variant='raised'
-              color='primary'
+        <Paper className={classes.paper}>
+          <form onSubmit={this.handleSubmit}>
+            <TextField
+              name='title'
+              label='Title'
+              onChange={this.handleChange}
+              margin='normal'
+              required
+            />
+            <br />
+            <TextField
+              name='description'
+              label='Description'
+              onChange={this.handleChange}
+              margin='normal'
+              rowsMax={3}
+              multiline
+              fullWidth
+              required
+            />
+            <br />
+            <TextField
+              select
+              label='Category'
+              name='category'
+              value={category}
+              onChange={this.handleChange}
+              margin='normal'
+              required
             >
-              Create
-            </Button>
-            <Button
-              component={Link}
-              to='/explore'
-              className={classes.button}
-              variant='raised'
-              color='error'
-            >
-              Cancel
-            </Button>
-          </Typography>
-        </form>
-      </Paper>
+              <MenuItem key={2} value={'miscellaneous'}>
+                Miscellaneous
+              </MenuItem>
+              {/*currencies.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))*/}
+            </TextField>
+            <br />
+            <FormControl component='fieldset' margin='normal'>
+              <FormHelperText>Access</FormHelperText>
+              <FormGroup>
+                <FormControlLabel
+                  control={
+                    <Switch
+                      name='access'
+                      color='secondary'
+                      checked={access}
+                      onChange={this.handleSwitch}
+                      value='public'
+                    />
+                  }
+                  label={access ? 'Public' : 'Private'}
+                />
+              </FormGroup>
+            </FormControl>
+
+            <Typography align='center'>
+              <Button
+                type='submit'
+                className={classes.button}
+                variant='raised'
+                color='primary'
+              >
+                Create
+              </Button>
+              <Button
+                component={Link}
+                to='/explore'
+                className={classes.button}
+                variant='raised'
+              >
+                Cancel
+              </Button>
+            </Typography>
+          </form>
+        </Paper>
+      </div>
     );
   }
 }
