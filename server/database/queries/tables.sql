2da991b5fe4a97f8e51d44143be235dea2a49c5c
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-03-24 12:15:21 -0400

    Added more table structures.

--- server/database/queries/tables.sql
+++ server/database/queries/tables.sql
@@ -7,7 +7,30 @@ CREATE TABLE users (
 	provider VARCHAR(15) DEFAULT 'email',
 	display_name VARCHAR(50),
 	actual_name JSON,
-	joined TIMESTAMP DEFAULT current_timestamp
+	created TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE skeams (
+	id SERIAL PRIMARY KEY NOT NULL,
+	category INTEGER REFERENCES categories(id) NOT NULL,
+	template INTEGER REFERENCES templates(id),
+	description VARCHAR(250) NOT NULL,
+	access BOOLEAN NOT NULL,
+	structure JSON,
+	edited TIMESTAMP DEFAULT current_timestamp,
+	created TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE templates (
+	id SERIAL PRIMARY KEY NOT NULL,
+	category INTEGER REFERENCES categories(id) NOT NULL,
+	edited TIMESTAMP DEFAULT current_timestamp,
+	created TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE categories (
+	id SERIAL PRIMARY KEY NOT NULL,
+	label VARCHAR(50) NOT NULL
 );
 
 INSERT INTO users (email, password, display_name)
