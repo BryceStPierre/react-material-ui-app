db1e7a1fd3f637ec736c308a21f9a9234f5b14ff
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-06-10 11:16:52 -0400

    Updated again.

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var session = require('express-session');
+var session = require('express-session');
 
-// var passport = require('./server/passport');
+var passport = require('./server/passport');
 
-// var signin = require('./server/api/signin');
-// var register = require('./server/api/register');
-// var categories = require('./server/api/categories');
-// var skeam = require('./server/api/skeam');
+var signin = require('./server/api/signin');
+var register = require('./server/api/register');
+var categories = require('./server/api/categories');
+var skeam = require('./server/api/skeam');
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,15 +18,15 @@ var app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.use('/api/signin', signin(passport));
-// app.use('/api/register', register(passport));
-// app.use('/api/categories', categories);
-// app.use('/api/skeam', skeam(passport));
+app.use('/api/signin', signin(passport));
+app.use('/api/register', register(passport));
+app.use('/api/categories', categories);
+app.use('/api/skeam', skeam(passport));
 
 var db = require('./server/database');
 db.query('SELECT NOW()', (res) => {
