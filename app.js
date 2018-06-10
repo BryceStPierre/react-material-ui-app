b72cd1bc75bfe32a161d1778c59ff577737d744f
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-06-10 11:08:54 -0400

    Disconnected API for deployment test.

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
-var passport = require('./server/passport');
+// var passport = require('./server/passport');
 
-var signin = require('./server/api/signin');
-var register = require('./server/api/register');
-var categories = require('./server/api/categories');
-var skeam = require('./server/api/skeam');
+// var signin = require('./server/api/signin');
+// var register = require('./server/api/register');
+// var categories = require('./server/api/categories');
+// var skeam = require('./server/api/skeam');
 
 const PORT = 3001;
 
@@ -20,18 +20,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.use('/api/signin', signin(passport));
-app.use('/api/register', register(passport));
-app.use('/api/categories', categories);
-app.use('/api/skeam', skeam(passport));
+// app.use('/api/signin', signin(passport));
+// app.use('/api/register', register(passport));
+// app.use('/api/categories', categories);
+// app.use('/api/skeam', skeam(passport));
 
-var db = require('./server/database');
-db.query('SELECT NOW()', (res) => {
-  console.log(`PostgreSQL connected: ${res[0].now}.`);
-});
+// var db = require('./server/database');
+// db.query('SELECT NOW()', (res) => {
+//   console.log(`PostgreSQL connected: ${res[0].now}.`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port 3001...`);
