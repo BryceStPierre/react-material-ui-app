da1d4884fd29c305cf433d2bb560af7253145a7d
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-06-10 11:12:20 -0400

    Another deployment test.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+// var session = require('express-session');
 
 // var passport = require('./server/passport');
 
@@ -18,7 +18,7 @@ var app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+// app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
 // app.use(passport.initialize());
 // app.use(passport.session());
@@ -28,10 +28,10 @@ app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: fals
 // app.use('/api/categories', categories);
 // app.use('/api/skeam', skeam(passport));
 
-// var db = require('./server/database');
-// db.query('SELECT NOW()', (res) => {
-//   console.log(`PostgreSQL connected: ${res[0].now}.`);
-// });
+var db = require('./server/database');
+db.query('SELECT NOW()', (res) => {
+  console.log(`PostgreSQL connected: ${res[0].now}.`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port 3001...`);
